cdf30bc587fce69d1bfa264df06b86d499cc37a8
jimfdavies <jimfdavies@googlemail.com>
2013-09-06 11:00:07 +0100

    added rs-create-security-group.sh (initial working) and added cloud_id to rs-show-security-groups

--- rs-create-security-group.sh
+++ rs-create-security-group.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-# rs-show-security-groups.sh
+# rs-create-security-group.sh
 # REQUIRES rs-login.sh to be run previously
 
 # REFERENCE:
@@ -9,21 +9,24 @@
 # http://reference.rightscale.com/api1.5/resources/ResourceSecurityGroups.html
 
 # Parameters:
-cloud_id="2"     # cloud_id: Id of the cloud in which the security group should be created e.g AWS EU: 2
+cloud_id="2"           # cloud_id: Id of the cloud in which the security group should be created e.g AWS EU: 2
+sg_group_name="SGTest" # ec2_security_group[aws_group_name], mandatory
+sg_group_desc="SGTest" # ec2_security_group[aws_description], mandatory
 
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
 
 case $rs_api_version in
-  "1.0") url="https://my.rightscale.com/api/acct/$rs_api_account_id/ec2_security_groups?cloud_id=$cloud_id"
-         echo "[API 1.0] GET: $url"
-		 result=$(curl -s -S -i -k -H X-API-VERSION:$rs_api_version -b $rs_api_cookie $url 2>&1)
+  "1.0") url="https://my.rightscale.com/api/acct/$rs_api_account_id/ec2_security_groups"
+         echo "[API 1.0] POST: $url"
+		 result=$(curl -s -S -i -k -H X-API-VERSION:$rs_api_version -b $rs_api_cookie \
+		 -d "ec2_security_group[aws_group_name]=$sg_group_name" \
+		 -d "ec2_security_group[aws_description]=$sg_group_desc" \
+		 -d "cloud_id=$cloud_id" \
+		 $url 2>&1)
 		 ;;
   "1.5") 
          #echo "[API 1.5] GET: $url"
-		 #rs_cloud_id="2373" # e.g. 2373: Rackspace Open Cloud - London
-         #url="https://my.rightscale.com/api/clouds/$rs_cloud_id/security_groups.xml"
-		 #result=$(curl -s -S -i -k -H X_API_VERSION:$rs_api_version -b $rs_api_cookie -X GET $url 2>&1)
 	     ;;
       *) echo ERROR: rs_api_version not set
 	     exit 1
@@ -31,7 +34,7 @@ case $rs_api_version in
 esac
 
 case $result in
-	*'200 OK'*)
+	*'201 CREATED'*)
 		echo "$result"
 		exit
 	;;
